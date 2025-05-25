cd979ab3a605504e19d73ae2b062edb78028015c
dandy <dandyincorp1@gmail.com>
2025-05-25 18:49:49 +0800

    first commit

---
+++ src/test/java/com/project/adaptationflow/AdaptationFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.adaptationflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptationFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
