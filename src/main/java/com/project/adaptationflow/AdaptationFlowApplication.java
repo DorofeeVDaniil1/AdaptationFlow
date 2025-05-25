0d6608e580df4a91cfdf0e3b58415307c3457868
dandy <dandyincorp1@gmail.com>
2025-05-25 18:51:05 +0800

    Start

--- src/main/java/com/project/adaptationflow/AdaptationFlowApplication.java
+++ src/main/java/com/project/adaptationflow/AdaptationFlowApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdaptationFlowApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdaptationFlowApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptationFlowApplication.class, args);
+    }
 
 }
